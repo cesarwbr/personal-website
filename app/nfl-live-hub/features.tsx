0eb8725dd3c57c3d8920f31b65157bf68409183d
Cesar William <cesarwbr@gmail.com>
2024-04-18 12:13:56 -0700

    fix: change png to webp

--- app/nfl-live-hub/features.tsx
+++ app/nfl-live-hub/features.tsx
@@ -5,32 +5,32 @@ export function Features() {
   const features = [
     {
       title: "HIGHLIGHT VIDEOS",
-      image: "/nfl-live-hub/highlights-screenshot.png",
+      image: "/nfl-live-hub/highlights-screenshot.webp",
       description:
         "Explore the highlights of every NFL game. Watch highlights of past games, live highlights as they happen, or stay tuned for the latest games.",
     },
     {
       title: "REAL-TIME SCOREBOARD",
-      image: "/nfl-live-hub/main-screenshot.png",
+      image: "/nfl-live-hub/main-screenshot.webp",
       description:
         "Stay updated with live scores from every NFL game. Our dynamic scoreboard brings you the latest from the field, ensuring you never miss a play.",
     },
     {
       title: "LIVE FAN CHAT",
-      image: "/nfl-live-hub/chat-screenshot.png",
+      image: "/nfl-live-hub/chat-screenshot.webp",
       description:
         "Join a vibrant community of NFL enthusiasts! Engage in real-time discussions about ongoing games, share predictions, celebrate victories, or analyze plays with fellow fans.",
     },
 
     {
       title: "STANDINGS TRACKER",
-      image: "/nfl-live-hub/standings-screenshot.png",
+      image: "/nfl-live-hub/standings-screenshot.webp",
       description:
         "Easily view the current NFL standings, sorted by conference and division. Track your team&apos;s progress throughout the season with just a click.",
     },
     {
       title: "GAME PREVIEWS & RECAPS",
-      image: "/nfl-live-hub/recap-screenshot.png",
+      image: "/nfl-live-hub/recap-screenshot.webp",
       description:
         "Dive into comprehensive game previews, complete with expert analysis and predictions. After the game, relive the action with our detailed recap videos.",
     },
