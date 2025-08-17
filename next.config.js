d6f60bbaedacfcf1fad70ddc1efc5ca2ce364d9b
Cesar William <cesarwbr@gmail.com>
2025-08-16 23:01:33 -0700

    fix: redirect nfl-live-hub to the official website

--- next.config.js
+++ next.config.js
@@ -3,24 +3,24 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.medium.com',
-        port: '',
+        protocol: "https",
+        hostname: "**.medium.com",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: 'i.scdn.co',
-        port: '',
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: 't0.gstatic.com',
-        port: '',
+        protocol: "https",
+        hostname: "t0.gstatic.com",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
       },
     ],
     deviceSizes: [320, 340, 660, 800],
@@ -28,17 +28,26 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/privacy',
-        destination: '/privacy.html',
+        source: "/privacy",
+        destination: "/privacy.html",
       },
       {
-        source: '/legal-ai-privacy-policy',
-        destination: '/legal-ai-privacy-policy.html',
+        source: "/legal-ai-privacy-policy",
+        destination: "/legal-ai-privacy-policy.html",
       },
       {
-        source: '/terms-of-service',
-        destination: '/terms-of-service.html',
+        source: "/terms-of-service",
+        destination: "/terms-of-service.html",
       },
-    ]
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "nfl-live-hub",
+        destination: "https://nfllivehub.com/",
+        permanent: true,
+      },
+    ];
   },
 };
